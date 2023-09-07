77e380f043d5e34de2b937496fbe29dc535cf2b7
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2023-09-07 15:28:04 +0300

    Initial Commit

---
+++ MealTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MealTime
+//
+//  Created by Lera Savchenko on 7.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
