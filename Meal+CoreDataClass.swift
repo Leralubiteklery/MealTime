8a2af4d8918b18884f82119a9c5b59e1382a8fe0
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2023-09-07 17:48:29 +0300

    Created model.

---
+++ Meal+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Meal+CoreDataClass.swift
+//  MealTime
+//
+//  Created by Lera Savchenko on 7.09.23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Meal)
+public class Meal: NSManagedObject {
+
+}
---
+++ User+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  User+CoreDataClass.swift
+//  MealTime
+//
+//  Created by Lera Savchenko on 7.09.23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(User)
+public class User: NSManagedObject {
+
+}
